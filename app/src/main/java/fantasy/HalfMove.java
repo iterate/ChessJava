a25f40772d0b5f601a873266989a9dcfc50a9473
Anders Haugeto <haugeto@iterate.no>
2023-04-10 09:23:43 +0200

    Refactoring

--- app/src/main/java/fantasy/HalfMove.java
+++ app/src/main/java/fantasy/HalfMove.java
@@ -23,6 +23,10 @@ public class HalfMove {
 
 	public Type promotedTo = null;
 
+	public HalfMove() {
+
+	}
+
 	public HalfMove(Castle castle) {
 		this.castle = castle;
 	}
@@ -32,55 +36,40 @@ public class HalfMove {
 		this.to = to.toLowerCase();
 	}
 
-	static HalfMove parse(String target) {
+	public static HalfMove parse(String target) {
 
 		if (Castle.KING_SIDE.notation.equals(target))
 			return new HalfMove(Castle.KING_SIDE);
 		else if (Castle.QUEEN_SIDE.notation.equals(target))
 			return new HalfMove(Castle.QUEEN_SIDE);
 
-		Type type = null;
-		boolean check = false;
-		boolean checkmate = false;
-		Type promotedTo = null;
-		Character fromFile = null;
-		Integer fromRank = null;
-		String square = "";
-		boolean capture = false;
-
 		Matcher matcher = ALGEBRAIC_NOTATION.matcher(target);
 		if (!matcher.matches())
 			throw new ChessException("Cannot parse: \"" + target + "\"");
 
+		HalfMove result = new HalfMove();
 		if (matcher.group(1) == null)
-			type = Type.PAWN;
+			result.type = Type.PAWN;
 		else
-			type = Type.parse(matcher.group(1));
+			result.type = Type.parse(matcher.group(1));
 
 		if (matcher.group(2) != null)
-			fromFile = matcher.group(2).charAt(0);
+			result.fromFile = matcher.group(2).charAt(0);
 
 		if (matcher.group(3) != null)
-			fromRank = Integer.parseInt(matcher.group(3));
+			result.fromRank = Integer.parseInt(matcher.group(3));
 
-		square = matcher.group(5);
+		result.to = matcher.group(5);
 
-		capture = matcher.group(4) != null;
+		result.capture = matcher.group(4) != null;
 
 		if (matcher.group(6) != null)
-			promotedTo = Type.parse(matcher.group(6).substring(1));
+			result.promotedTo = Type.parse(matcher.group(6).substring(1));
 
-		check = "+".equals(matcher.group(7));
-		checkmate = "#".equals(matcher.group(7));
+		result.check = "+".equals(matcher.group(7));
+		result.checkmate = "#".equals(matcher.group(7));
 
-		HalfMove halfMove = new HalfMove(type, square);
-		halfMove.capture = capture;
-		halfMove.fromFile = fromFile;
-		halfMove.fromRank = fromRank;
-		halfMove.check = check;
-		halfMove.promotedTo = promotedTo;
-		halfMove.checkmate = checkmate;
-		return halfMove;
+		return result;
 	}
 
 	@Override
